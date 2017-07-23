9a4e332142715d5067cdd822f1825c314e531c0e
mitul jindal <mituljindal@gmail.com>
2017-07-23 18:25:10 +0530

    Initial Commit

---
+++ Rock-Paper-Scissors/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rock-Paper-Scissors
+//
+//  Created by Mitul Jindal on 23/07/17.
+//  Copyright © 2017 mitul jindal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
