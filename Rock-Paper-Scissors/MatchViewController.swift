4b94297309f234fd8585012c1cbe1bc885f048c8
mitul jindal <mituljindal@gmail.com>
2017-07-24 01:06:13 +0530

    Completed and added comments

--- Rock-Paper-Scissors/MatchViewController.swift
+++ Rock-Paper-Scissors/MatchViewController.swift
@@ -5,31 +5,40 @@
 //  Created by Mitul Jindal on 23/07/17.
 //  Copyright © 2017 mitul jindal. All rights reserved.
 //
+// This is the first view
 
 import UIKit
 
 class MatchViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
     
+    // For connecting data from this ViewController to the ResultViewController
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let controller = segue.destination as! ResultViewController // .destination stores the information of ResultViewController in controller
+        
+        if segue.identifier == "code" { // if paper is pressed
+            controller.choice = 1
+        } else if segue.identifier == "noCode" { // if scissors is pressed
+            controller.choice = 2
+        }
+        
+        controller.computerChoice = Int(arc4random_uniform(3)) // result for computer
+    }
 
-    @IBAction func rockButtonPressed(_ sender: Any) {
+    @IBAction func rockButtonPressed(_ sender: Any) { // if rock is pressed
         
         let controller: ResultViewController
-        controller = storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as! ResultViewController
+        controller = storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as! ResultViewController // instantiating controller with ResultViewController
         
-        controller.result = 0
-        controller.computerResult = Int(arc4random_uniform(3))
+        controller.choice = 0
+        controller.computerChoice = Int(arc4random_uniform(3)) // result for computer
         
-        self.present(controller, animated: true, completion: nil)
+        self.present(controller, animated: true, completion: nil) // presenting ResultViewController with code
     }
 
-    @IBAction func paperButtonPressed(_ sender: Any) {
+    @IBAction func paperButtonPressed(_ sender: Any) { // if paper is pressed
         
-        performSegue(withIdentifier: "code", sender: self)
+        performSegue(withIdentifier: "code", sender: self) // presenting ResultViewController with code and segue
     }
 
 }
--- Rock-Paper-Scissors/ResulViewController.swift
+++ Rock-Paper-Scissors/ResulViewController.swift
@@ -5,49 +5,55 @@
 //  Created by Mitul Jindal on 23/07/17.
 //  Copyright © 2017 mitul jindal. All rights reserved.
 //
+//This is the final view
 
 import UIKit
 
 class ResultViewController: UIViewController {
     
-    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var resultLabel: UILabel! // creating label instance
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageView: UIImageView! // creating imageview instance
     
-    var result: Int?
-    var computerResult: Int?
+    var choice: Int? // variable for player choice
+    var computerChoice: Int? //variable for computer choice
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         
-        compileResult(self.result!, self.computerResult!)
+        compileResult(self.choice!, self.computerChoice!) // changing view with result
     }
     
     func compileResult(_ player: Int, _ computer: Int) {
-        var final: Int = 3
+        var final: Int = -1 // default value (to remove error)
         switch (player, computer) {
-        case (0,0):
+        case (0,0), (1,1), (2,2): // tie result
             imageView.image = #imageLiteral(resourceName: "itsATie")
             resultLabel.text = "Try Again!"
             return
-        case (0,1), (1,0):
+        case (0,1), (1,0): // paper wins
             imageView.image = #imageLiteral(resourceName: "PaperCoversRock")
             final = 1
-        case (1,2), (2,1):
+        case (1,2), (2,1): // scissors win
             imageView.image = #imageLiteral(resourceName: "ScissorsCutPaper")
             final = 2
-        default:
+        case (0,2), (2,0): // rock wins
             imageView.image = #imageLiteral(resourceName: "RockCrushesScissors")
             final = 0
+        default: // in case of problem
+            print("problem")
+            return
         }
         
-        if player == final {
+        if player == final { // if player wins
             resultLabel.text = "Hurray! You win"
-        } else {
+        } else { // if player loses
             resultLabel.text = "Alas, you lost!"
         }
     }
     
+    @IBAction func playAgain(_ sender: Any) { // dismissing the result
+        self.dismiss(animated: true, completion: nil)
+    }
     
 }
